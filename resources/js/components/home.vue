ee51a26bf2c0011903e7daac917756b9b3950419
developernaeem <naeemislam.pa@gmail.com>
2020-02-25 14:05:51 +0600

    Home Component Change

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<h1 class="mt-4">Dashboard</h1>
+		<h1 class="mt-4">Welcome Dashboard</h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item active">Dashboard</li>
         </ol>
